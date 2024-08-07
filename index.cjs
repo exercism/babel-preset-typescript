43019345148fd57be06d15f0f7826fc6102f09cd
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2024-08-07 22:04:51 +0200

    Pin core-js to 3.38.x

--- index.cjs
+++ index.cjs
@@ -8,7 +8,7 @@ module.exports = function (_api, opts = {}) {
             node: 'current',
           },
           useBuiltIns: 'usage',
-          corejs: opts['corejs'] || '3.37',
+          corejs: opts['corejs'] || '3.38',
         },
       ],
       require('@babel/preset-typescript'),
